d2cd4f2e47a046bbb93cc518b3a95411fa9bdf31
Narendra122812 <narendrakumar122812@gmail.com>
2024-08-11 01:16:36 +0530

    Project Setup

---
+++ src/main/java/SwagLabs_Automation/App.java
@@ -0,0 +1,13 @@
+package SwagLabs_Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
