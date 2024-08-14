4bdd5d6b7460a5bc8e0c24a6e0920b5092fbb9ec
Narendra122812 <narendrakumar122812@gmail.com>
2024-08-15 02:46:16 +0530

    Test Script abd Modify unused file.

--- src/test/java/com/swaglabs/pages/BaseTest.java
+++ src/test/java/com/swaglabs/pages/BaseTest.java
@@ -1,12 +1,27 @@
 package com.swaglabs.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import com.swaglabs.utility.BrowserFactory;
+import com.swaglabs.utility.ConfigDataProvider;
 
 public class BaseTest {
 	
+  /**
+   * Global Variable
+   */
+	public WebDriver driver;
+	public ConfigDataProvider config = new ConfigDataProvider();
+	
 	@BeforeClass
-	void tc() {
-		
+	public void setup() {
+		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingUrl());
 	}
-
+	
+	@AfterClass
+	public void tearDown() {
+		BrowserFactory.quitBrowser(driver);
+	}
 }
---
+++ src/test/java/com/swaglabs/testcases/Testcase.java
@@ -0,0 +1,14 @@
+package com.swaglabs.testcases;
+
+import org.testng.annotations.Test;
+
+import com.swaglabs.pages.BaseTest;
+
+public class Testcase {
+	
+	@Test
+	void testcheck() {
+		BaseTest obj = new BaseTest();
+	}
+
+}
