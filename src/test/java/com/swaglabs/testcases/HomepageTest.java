f1a9570c52bdad7408ef54f20f5ba0393374295b
Narendra122812 <narendrakumar122812@gmail.com>
2024-09-06 00:12:32 +0530

    Update HomePageTest.java File

--- src/test/java/com/swaglabs/testcases/HomepageTest.java
+++ src/test/java/com/swaglabs/testcases/HomepageTest.java
@@ -1,9 +1,27 @@
 package com.swaglabs.testcases;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import com.swaglabs.pages.BaseTest;
+import com.swaglabs.pages.LoginPage;
+import com.swaglabs.pages.HomePage;
 
 public class HomepageTest extends BaseTest {
 	
-
+	LoginPage loginPage = new LoginPage(driver);
+	HomePage homePage = new HomePage(driver);
+	
+	@BeforeMethod
+	public void setupmethod() {
+		loginPage.enterUsername("standard_user");
+		loginPage.enterPassword("secret_sauce");
+		loginPage.clickLogin();
+	}
+	
+	@Test
+	public void validateHomePage() {
+		
+	}
    
 }
