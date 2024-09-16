04713285dfc86d9367e84c7453ca0495b11b8f3b
Narendra122812 <narendrakumar122812@gmail.com>
2024-09-16 23:30:15 +0530

    Run Testscript

--- src/test/java/com/swaglabs/testcases/HomepageTest.java
+++ src/test/java/com/swaglabs/testcases/HomepageTest.java
@@ -1,31 +1,8 @@
 package com.swaglabs.testcases;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import com.swaglabs.pages.BaseTest;
-import com.swaglabs.pages.LoginPage;
-import com.swaglabs.pages.HomePage;
 
 public class HomepageTest extends BaseTest {
 	
-	LoginPage loginPage = new LoginPage(driver);
-	HomePage homePage = new HomePage(driver);
-	
-	@BeforeMethod
-	public void setupmethod() {
-		loginPage.enterUsername("standard_user");
-		loginPage.enterPassword("secret_sauce");
-		loginPage.clickLogin();
-	}
-	
-	@Test
-	public void validateHomePage() {
-		if (homePage.isTitleHomePageDisplayed()) {
-			System.out.println("Displayed Succesfully Home Page.");
-		}else {
-			System.out.println("Not Displayed Home Page.");
-		}
-	}
-   
+	   
 }
