dcfadf34f15e23bea371b23011786b0a8b913c5f
Narendra122812 <narendrakumar122812@gmail.com>
2025-04-03 17:17:19 +0530

    Modify HomePageTest.java File

--- src/test/java/com/swaglabs/testcases/HomepageTest.java
+++ src/test/java/com/swaglabs/testcases/HomepageTest.java
@@ -1,6 +1,6 @@
 package com.swaglabs.testcases;
 
-import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.swaglabs.pages.BaseTest;
@@ -9,20 +9,26 @@ import com.swaglabs.pages.LoginPage;
 
 public class HomepageTest extends BaseTest {
 
-	private HomePage homePage;
-
-	public WebDriver driver;
-
-	LoginPage lp = new LoginPage(driver);
-	HomePage hp = new HomePage(driver);
+	private LoginPage lp;
+	private HomePage hp;
 
 	public String userName = "standard_user";
 	public String password = "secret_sauce";
 
+	@BeforeMethod
+	public void setupTest() {
+		lp = new LoginPage(driver);
+		hp = new HomePage(driver);
+
+		lp.enterUsername(userName);
+		lp.enterPassword(password);
+		lp.clickLogin();
+	}
+
 	@Test
 	public void isHomepageDisplayed() {
 		if (hp.isTitleHomePageDisplayed()) {
-			System.out.println("HomePage is Displyed !!!");
+			System.out.println("HomePage is Displayed !!!");
 		} else {
 			System.out.println("HomePage Not Displayed !!!");
 		}
